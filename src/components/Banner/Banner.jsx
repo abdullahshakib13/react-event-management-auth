e9fe4bf539d02b515b14c9ea389e99a6af951c37
Abdullah Shakib <abdullahshakib13@gmail.com>
2023-10-08 14:42:44 +0600

    added banner in home page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,20 @@
 import image from "../../assets/cover_photo.jpg";
 const Banner = () => {
     return (
-        <div >
-            <img className="w-[1320px] h-[700px]" src={image} alt="" />
-        </div>
+        // <div >
+        //     <img className="w-[1320px] h-[600px]" src={image} alt="" />
+        // </div>
+
+        <div className="hero min-h-screen" style={{backgroundImage: 'url(https://i.ibb.co/NCBYZCF/cover-photo.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-4xl font-bold">Event Management</h1>
+      <p className="mb-5">Mastering the Art of Event Management: Creating Memorable Experiences</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
     );
 };
 
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,8 +4,8 @@ const Services = ({ services }) => {
   console.log(services);
     return (
         <div>
-            <h2 className="text-center">Our Services</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            <h2 className="text-3xl font-semibold text-center">Our Services</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5 gap-5">
           {
           services.map(service=><ServicesCard key={service.id} service={service}></ServicesCard>)
             }
